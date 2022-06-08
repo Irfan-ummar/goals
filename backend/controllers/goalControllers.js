803124c7a7340a2ab04deb810f866fde79b9f9ea
Muhammed Irfan <m.irfanumar007@gmail.com>
2022-06-08 18:32:50 +0530

    added crud operations fpr goal in mongodb

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,35 +1,66 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 //@desc GET gaols
 //@route GET/api/goals
 // @access Private
-const getGoals = asynHandler(async(req, res)=> {
-  res.status(200).json({message : 'get goal'})
+const getGoals = asyncHandler(async(req, res)=> {
+  const goals = await Goal.find()
+
+  res.status(200).json(goals)
 })
 
 //@desc POST gaols
 //@route POST/api/goals
 // @access Private
-const setGoals = asynHandler(async(req, res)=> {
+const setGoals = asyncHandler(async(req, res)=> {
     if (!req.body.text){
         res.status(400)
         throw new Error('Please add a text')
     }
-    res.json({message : "set all goalsss"})
+    const goal = await Goal.create({
+      text: req.body.text
+    })
+
+    res.json(goal)
   })
 
 //@desc PUT gaols
 //@route PUT/api/goals/:id
 // @access Private
-const updateGoals = asynHandler(async(req, res)=> {
-    res.json({message : `Update goal ${req.params.id}`})
+const updateGoals = asyncHandler(async(req, res)=> {
+    
+    const goal = await Goal.findById(req.params.id)
+    if(!goal){
+      res.status(400)
+      throw new Error('goal not found')
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,
+      {
+        new:true,
+      })
+
+    res.json(updateGoal)
   })
 
 //@desc DELETE gaols
 //@route GET/api/goals/:id
 // @access Private
-const delteGoals = asynHandler(async(req, res)=> {
-    res.json({message : `delete goal ${req.params.id}`})
+const delteGoals = asyncHandler(async(req, res)=> {
+
+  const goal = await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new Error('goal not found')
+  }
+
+  await goal.remove()
+
+  // const deleteGoal = await Goal.findByIdAndRemove(req.params.id,req.body,)
+  // const deleteGoal = await Goal.findByIdAndDelete(req.params.id,req.body,)
+    res.json({id : req.params.id})
   })
 
 
